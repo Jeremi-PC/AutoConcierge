2e4fb730a900e59e1fc94690411025a0f8e508dc
Jeremi <redbadcat777@gmail.com>
2024-03-11 10:37:48 +0100

    first commit

---
+++ src/main/java/com/example/auto_concierge/AutoConciergeApplication.java
@@ -0,0 +1,13 @@
+package com.example.auto_concierge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoConciergeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoConciergeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/auto_concierge/AutoConciergeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auto_concierge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoConciergeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
